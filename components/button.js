ab2e53c93ea13691748a14a9ef7751753d05c807
nour-msh <mshawrabnour@gmail.com>
2024-02-07 18:09:44 +0200

    Styling button component

---
+++ components/button.js
@@ -0,0 +1,32 @@
+import { View, Text, TextInput, StyleSheet, TouchableOpacity} from "react-native";
+
+const Button = ({title}) => {
+    return (
+        <View>
+
+        <TouchableOpacity style={styles.button}>
+            <Text style={styles.btnText}>{title}</Text>
+        </TouchableOpacity>
+         
+        </View>
+      );
+    };
+
+const styles = StyleSheet.create({
+    button:{
+width:167,
+height:48,
+borderRadius:10,
+backgroundColor:"#FE6778",
+
+
+    },
+    btnText:{
+        color:"white",
+        fontSize:16,
+        fontWeight:"bold",
+        textAlign:"center",
+paddingTop:"8%"
+    }});
+
+    export default Button

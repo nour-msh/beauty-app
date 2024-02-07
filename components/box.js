36ea47690fa189dc2e6cd5ef72d2329eef29be7b
nour-msh <mshawrabnour@gmail.com>
2024-02-07 21:25:41 +0200

    Adding box component

---
+++ components/box.js
@@ -0,0 +1,27 @@
+import { LinearGradient } from 'expo-linear-gradient';
+import { StyleSheet,View } from 'react-native';
+
+
+const Box= () => {
+    return(
+        <>
+        <LinearGradient colors={['#FFA4AE' ,'#FFA4AE', '#FFA4AE', '#FFA4AE']} style={styles.box}>
+        <View>
+        </View>
+        </LinearGradient>
+        </>
+        )
+};
+
+const styles = StyleSheet.create({
+
+    box:{
+        height:175,
+        width:375,
+        borderBottomRightRadius:50,
+        borderBottomLeftRadius:50
+        },
+}
+    )
+
+    export default Box

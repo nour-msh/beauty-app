0a79da4f9dd5150ae02b297c798f0cb692a37645
nour-msh <mshawrabnour@gmail.com>
2024-02-07 22:40:46 +0200

    Creating product card component

---
+++ components/productCard.js
@@ -0,0 +1,44 @@
+import { StyleSheet, View,Text} from "react-native";
+
+const Product = () =>{
+return(
+<View style={styles.productView}>
+
+        <View style={styles.productDetails}>
+
+        <Text style={{fontWeight:600}}>Title</Text>
+        <Text style={{fontWeight:600, color:"#FE6778"}}>$Price</Text>
+        </View>
+
+</View>
+        )
+}
+
+const styles= StyleSheet.create({
+    productView:{
+        height:170,
+        width:150,
+        display:"flex",
+        // justifyContent:"center",
+        // alignItems:"center",
+        backgroundColor:"white",
+        borderRadius:15,
+        // marginBottom:"5%"
+
+    },
+    productDetails:{
+        height:50,
+        width:150,
+        borderBottomRightRadius:10,
+        borderBottomLeftRadius:10,
+        backgroundColor:"#FFA4AE",
+        top:120,
+        padding:"5%",
+        textAlign:"left",
+
+
+    },
+  
+});
+
+export default Product

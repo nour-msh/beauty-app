8365dbe05fbd76ace134785261947d2b2dc9a297
nour-msh <mshawrabnour@gmail.com>
2024-02-07 18:09:23 +0200

    Styling input component

---
+++ components/input.js
@@ -0,0 +1,37 @@
+import { View, Text, TextInput, StyleSheet} from "react-native";
+
+const Input = ({label}) => {
+    return (
+        <View>
+
+        <Text style={styles.label}>{label}</Text>
+          <TextInput
+            style={styles.input}
+            // onChangeText={onChangeNumber}
+            // value={number}
+            // placeholder="Placeholder"
+            keyboardType="default"
+            />
+        </View>
+      );
+    };
+
+const styles = StyleSheet.create({
+    input: {
+      height: 50,
+    //   margin: 12,
+      borderWidth:0,
+      borderRadius:8,
+      width:325,
+      padding: 10,
+      borderWidth:1,
+  borderColor: "#FFE1E4",
+   
+    },
+    label:{
+        paddingVertical:"2%",
+    }
+  });
+
+  export default Input;
+  

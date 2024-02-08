6b545275f20dc417ef0b185b2ae13722fbbdbb12
nour-msh <mshawrabnour@gmail.com>
2024-02-09 00:38:05 +0200

    Adding layout screen for services screen

---
+++ app/(tabs)/home/_layout.js
@@ -0,0 +1,16 @@
+import { Stack } from "expo-router";
+
+export default function _layout(){
+return < Stack screenOptions={{
+    headerShown:false,
+    headerStyle:{
+        backgroundColor: "pink"
+    },
+    headerTintColor:
+        "white",
+}}>
+
+<Stack.Screen name="index"/>
+<Stack.Screen name="services" />
+</Stack>
+}

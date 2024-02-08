aaad7befbaf1c7586396286b4316c1dec2df9ec0
nour-msh <mshawrabnour@gmail.com>
2024-02-09 00:40:09 +0200

    Adding clickable cards to home screen

--- app/(tabs)/home/index.js
+++ app/(tabs)/home/index.js
@@ -1,22 +1,30 @@
-import {Text, View, Button, StyleSheet, ScrollView} from "react-native";
+import {Text, View, Button, StyleSheet, ScrollView, TouchableOpacity} from "react-native";
 import Box from "../../../components/box";
 import Category from "../../../components/category";
 import Service from "../../../components/serviceCard";
+import { Link, useRouter } from "expo-router";
+import { useNavigation } from "expo-router";
 
 export default function Home() {
-
-  return (
-      <>
+    
+ const router = useRouter();
+     return(
+        <>
       <View>
       <Box/>
       </View>
+            <View style={{bottom:120, paddingLeft:"5%"}}>
+
+            <Text style={{fontWeight:300, fontSize:22, color:"white"}}>Welcome to</Text>
+            <Text style={{fontWeight:800, fontSize:30 ,color:"white"}}>Beauty Salon</Text>
+            </View>
         <View style={styles.container}>
             <Text style={{fontWeight:600}}> Popular Services</Text>
             <ScrollView horizontal={true}>
 
             <View style={styles.slider}>
 
-                <Category textCat="All"/>
+                <Category textCat="All" />
                 <Category textCat="Skin"/>
                 <Category textCat="Hair"/>
                 <Category textCat="Body"/>
@@ -27,24 +35,44 @@ export default function Home() {
             <ScrollView showsVerticalScrollIndicator={false}>
 
             <View style={styles.scroller}>
+                
+           <TouchableOpacity onPress={()=>{ router.push("/home/services")
+                }}>
                 <Service/>
+               </TouchableOpacity>  
+           <TouchableOpacity onPress={()=>{ router.push("/home/services")
+                }}>
                 <Service/>
+               </TouchableOpacity>  
+           <TouchableOpacity onPress={()=>{ router.push("/home/services")
+                }}>
                 <Service/>
+               </TouchableOpacity>  
+           <TouchableOpacity onPress={()=>{ router.push("/home/services")
+                }}>
                 <Service/>
+               </TouchableOpacity>  
+           <TouchableOpacity onPress={()=>{ router.push("/home/services")
+                }}>
                 <Service/>
+               </TouchableOpacity>  
+           <TouchableOpacity onPress={()=>{ router.push("/home/services")
+                }}>
                 <Service/>
-                <Service/>
-                <Service/>
+               </TouchableOpacity>  
+            
             </View>
             </ScrollView>
         </View>
       </>
-  );
+
+)
 }
 
 const styles= StyleSheet.create({
     container:{
         padding:"5%",
+        bottom:50
 
 
     },

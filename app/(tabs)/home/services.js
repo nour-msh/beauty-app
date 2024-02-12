6843ee0b244948711adbb527337c8f1480ea6a34
nour-msh <mshawrabnour@gmail.com>
2024-02-12 13:26:19 +0200

    Adding navigation to services button

--- app/(tabs)/home/services.js
+++ app/(tabs)/home/services.js
@@ -2,13 +2,24 @@
 import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
 import Button from "../../../components/button";
 import ServiceButton from "../../../components/serviceButton";
+import {useNavigation} from "expo-router";
+import { Ionicons } from '@expo/vector-icons';
+
+
 
 export default function Services () {
+
+    const navigation = useNavigation();
+    const navigateApp = ()=>{ navigation.navigate("bookAppointment")}
     return(
         <View style={styles.serviceContainer}>
             <View style={styles.imageContainer}>
 
-</View>
+             <View style={{display:"flex", flexDirection:"row",justifyContent:"space-between",padding:"5%", bottom:120}}>
+                <Ionicons name="chevron-back" size={24} color="black" style={{top:"5%"}}/>
+                <Ionicons name="heart-circle" size={50} color="black" />
+            </View>
+        </View>
 
 <View style={styles.infoContainer}>
 <Text style={{fontWeight:600, paddingBottom:10, paddingTop:10}}>Ingredients</Text>
@@ -25,10 +36,12 @@ export default function Services () {
 <Text style={{fontWeight:600 ,paddingBottom:10, paddingTop:10}}>About</Text>
 <Text>Gently pat your skin dry with a clean towel. Avoid harsh rubbing.</Text>
 <Text style={{fontWeight:600, paddingBottom:10, paddingTop:10}}>Reviews</Text>
-<Text>I recently had the pleasure of indulging in a beauty service that left me feeling like a whole new person. Read More</Text>
+<Text>I recently had the pleasure of indulging in a beauty service that left me feeling like a whole new person.<Text style={{color:"#FE6778"}}> Read More</Text> </Text>
 <View style={{display:"flex", justifyContent:"center", alignItems:"center", paddingTop:"5%"}}>
 
-<Button title="Book Service"/>
+
+<Button title="Book Service" onPress={navigateApp}/>
+    
 </View>
 </View>
     </View>

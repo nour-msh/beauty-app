5903164f9ac1c747b2c2891ffd0de485be9e28f8
nour-msh <mshawrabnour@gmail.com>
2024-02-19 15:36:36 +0200

    Adding product items mock data

---
+++ mockdata/productItems.js
@@ -0,0 +1,17 @@
+
+export const productItems = [
+  { id: 1, name: 'MAC', price: '$20', imageUrl: require('../assets/mac.png'), additions: ["Ruby Woo", "Velvet Teddy", "Lady Danger"] },
+  { id: 2, name: 'NARS', price: '$20', imageUrl: require('../assets/nars.png'), additions: ["Dolce Vita", "Orgasm", "Schiap"] },
+  {
+    id: 3, name: 'Maybelline', price: '$20', imageUrl: require('../assets/maybelline.png'), additions: ["Touch of Spice", "Pink Punch", "Red Revival"]
+  },
+  { id: 4, name: 'Revlon', price: '$20', imageUrl: require('../assets/revlon.png'), additions: ["Fire and Ice", "Cherries in the Snow", "Pink in the Afternoon"] },
+  { id: 5, name: "L'Oreal", price: '$20', imageUrl: require("../assets/L'Oreal.png"), additions: ["Fairest Nude", "Toasted Almond", "British Red",] },
+  {
+    id: 6, name: 'NYX', price: '$20', imageUrl: require('../assets/NYX.png'), additions: [
+      "Soft Matte Lip Cream in Stockholm",
+      "Liquid Suede Cream Lipstick in Tea & Cookies",
+      "Matte Lipstick in Alabama",
+    ]
+  },
+];

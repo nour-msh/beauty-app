e374e25f85a6aa59c1d89e82a68dfcd5af236fdf
nour-msh <mshawrabnour@gmail.com>
2024-02-19 15:36:53 +0200

    Adding categories mock data

---
+++ mockdata/categories.js
@@ -0,0 +1,7 @@
+export const categories = [
+  { id: 1, cat: "All", imageUrl: require('../assets/filter-all.png') },
+  { id: 2, cat: "Skin", imageUrl: require('../assets/filter-skin.png') },
+  { id: 3, cat: "Hair", imageUrl: require('../assets/filter-hair.png') },
+  { id: 4, cat: "Nails", imageUrl: require('../assets/filter-nails.png') },
+  { id: 5, cat: "Body", imageUrl: require('../assets/filter-body.png') },
+];

98abf977434b19a0eb794732143fbc9b59f80497
nour-msh <mshawrabnour@gmail.com>
2024-02-19 15:43:37 +0200

    Creating appointments screeen layout

---
+++ app/(tabs)/appointments/_layout.js
@@ -0,0 +1,15 @@
+import { Stack } from "expo-router";
+
+export default function _layout(){
+return < Stack screenOptions={{
+    headerShown:false,
+    headerStyle:{
+        backgroundColor: "pink"
+    },
+    headerTintColor:
+        "white",
+}}>
+
+<Stack.Screen name="index"/>
+</Stack>
+}

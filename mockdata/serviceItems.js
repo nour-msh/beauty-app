7923afddaa48e338d2bbda802bb69075dccbc346
nour-msh <mshawrabnour@gmail.com>
2024-02-19 15:36:21 +0200

    Adding service items mock data

---
+++ mockdata/serviceItems.js
@@ -0,0 +1,28 @@
+export const serviceItems = [
+  { id: 1, name: 'Massage', price: '$20', imageUrl: require('../assets/massage.png'), additions: ["Swedish Massage", "Deep Tissue Massage", "Hot Stone Massage", "Aromatherapy Massage", "Thai Massage", "Sports Massage", "Prenatal Massage", "Reflexology", "Couples Massage", "Chair Massage"] },
+  { id: 2, name: 'Facial', price: '$20', imageUrl: require('../assets/facial.png'), additions: ["Deep Cleansing Facial", "Hydrating Facial", "Anti-Aging Facial", "Acne Facial", "Brightening Facial", "Microdermabrasion", "Chemical Peel", "LED Light Therapy", "Oxygen Facial", "Galvanic Facial"] },
+  {
+    id: 3, name: 'Highlights', price: '$20', imageUrl: require('../assets/highlight.png'), additions: [ "Full Head Highlights","Partial Highlights", "Balayage", "Ombre","Foilyage", "Babylights","Face-Framing Highlights","Lowlights","Root Shadow", "Toner","Glossing","Color Melt","Paneling","Chunking","Slicing","Dip Dye","Sun-Kissed Highlights","Lived-In Color","Money Piece Highlights",
+    ]
+  },
+  { id: 4, name: 'Facial', price: '$20', imageUrl: require('../assets/facial.png'), additions: ["Deep Cleansing Facial", "Hydrating Facial", "Anti-Aging Facial", "Acne Facial", "Brightening Facial", "Microdermabrasion", "Chemical Peel", "LED Light Therapy", "Oxygen Facial", "Galvanic Facial"] },
+  { id: 5, name: 'Massage', price: '$20', imageUrl: require('../assets/massage.png'), additions: ["Swedish Massage", "Deep Tissue Massage", "Hot Stone Massage", "Aromatherapy Massage", "Thai Massage", "Sports Massage", "Prenatal Massage", "Reflexology", "Couples Massage", "Chair Massage"] },
+  {
+    id: 6, name: 'Gellish', price: '$20', imageUrl: require('../assets/gellish.png'), additions: [
+      "Gel Manicure",
+      "Gel Pedicure",
+      "Gel Polish Removal",
+      "Gel Nail Extensions",
+      "Gel Overlay",
+      "French Gel Manicure",
+      "Color Gel Manicure",
+      "Nail Art with Gel Polish",
+      "Gel Nail Repair",
+      "Gel Nail Soak-Off",
+      "Builder Gel Application",
+      "Gel Top Coat Application",
+      "Gel Nail Extension Removal",
+      "Gel Nail Maintenance",
+    ]
+  },
+];

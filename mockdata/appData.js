77d634fba910594808e27d3e1a5f29e43f42c919
nour-msh <mshawrabnour@gmail.com>
2024-02-19 15:37:11 +0200

    Adding appointments mock data

---
+++ mockdata/appData.js
@@ -0,0 +1,10 @@
+export const appData = [
+    { id: 1, date: 'Monday 17th, Jan 2024' , time: "10:00 AM", service_name: "Massage", price:"$80"},
+    { id: 2, date: 'Monday 18th, Jan 2024' , time: "11:00 AM", service_name: "Facial", price:"$60" },
+    { id: 3, date: 'Monday 19th, Jan 2024' , time: "12:00 PM", service_name: "Highlight", price:"$45"},
+    { id: 5, date: 'Monday 21th, Jan 2024' , time: "2:00 PM", service_name: "Hair Removal", price:"$35"},
+    { id: 4, date: 'Monday 20th, Jan 2024' , time: "1:00 PM", service_name: "Manicure and Pedicure", price:"$40" },
+    { id: 5, date: 'Monday 21th, Jan 2024' , time: "2:00 PM", service_name: "Hair Removal", price:"$35"},
+    { id: 6, date: 'Monday 22th, Jan 2024' , time: "3:00 PM", service_name: "Eyebrow Grooming", price:"$10" }
+
+  ];
